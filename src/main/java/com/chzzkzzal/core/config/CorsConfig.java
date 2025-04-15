fe5ae54e850f39020d3c979333924be9c970f3e4
plan11plan <tjtfy7028@gmail.com>
2025-04-16 02:33:43 +0900

    refactor: cors 프론트 주소 변경

--- src/main/java/com/chzzkzzal/core/config/CorsConfig.java
+++ src/main/java/com/chzzkzzal/core/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.chzzkzzal.core.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,16 +9,20 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
+	@Value("${chzzkzzal.front}")
+	private String FRONT_DOMAIN;
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지 설정
-        config.addAllowedOrigin("http://localhost:3000"); // 모든 ip에 응답 허용
-        config.addAllowedHeader("*"); // 모든 헤더에 응답 허용
-        config.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청 허용
-        source.registerCorsConfiguration("/**", config); // 모든 경로에 위 설정 적용
-        return new CorsFilter(source);
-    }
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지 설정
+		// config.addAllowedOrigin("http://localhost:3000"); // 모든 ip에 응답 허용
+		config.addAllowedOrigin(FRONT_DOMAIN + ":3000"); // 모든 ip에 응답 허용
+
+		config.addAllowedHeader("*"); // 모든 헤더에 응답 허용
+		config.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청 허용
+		source.registerCorsConfiguration("/**", config); // 모든 경로에 위 설정 적용
+		return new CorsFilter(source);
+	}
 }
