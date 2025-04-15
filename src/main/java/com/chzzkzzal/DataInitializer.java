cf626b2571c7729beef6e3af811d8ab1ea299667
plan11plan <tjtfy7028@gmail.com>
2025-04-16 01:13:05 +0900

    refactor: 도메인주소 환경변수화

--- src/main/java/com/chzzkzzal/DataInitializer.java
+++ src/main/java/com/chzzkzzal/DataInitializer.java
@@ -1,5 +1,6 @@
 package com.chzzkzzal;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,9 +15,12 @@ public class DataInitializer implements CommandLineRunner {
 
 	private final StreamerService streamerService;
 
+	// application.yml에 설정한 값
+	@Value("${chzzkzzal.domain}")
+	private String domain;
+
 	@Override
 	public void run(String... args) throws Exception {
-		// 애플리케이션 시작 시 실행할 코드
 		도파();
 		랄로();
 		파카();
@@ -25,19 +29,20 @@ public class DataInitializer implements CommandLineRunner {
 
 	private void 괴물쥐() {
 		RegisterStreamerCommand command = new RegisterStreamerCommand(
-			"c7ded8ea6b0605d3c78e18650d2df83b",
+			"c7ded8ea6b0...",
 			"괴물쥐",
-			"http://localhost:8080/zzal/괴물쥐.jpg",
+			domain + "/zzal/괴물쥐.jpg", // <-- 이렇게 외부에서 주입한 값을 사용
 			290000
 		);
 		streamerService.register(command);
 	}
 
+	// 이하 동일
 	private void 도파() {
 		RegisterStreamerCommand command = new RegisterStreamerCommand(
-			"c847a58a1599988f6154446c75366523",
+			"c847a5....",
 			"도파99",
-			"http://localhost:8080/zzal/도파.jpg",
+			domain + "/zzal/도파.jpg",
 			114000
 		);
 		streamerService.register(command);
@@ -45,9 +50,9 @@ public class DataInitializer implements CommandLineRunner {
 
 	private void 랄로() {
 		RegisterStreamerCommand command = new RegisterStreamerCommand(
-			"3497a9a7221cc3ee5d3f95991d9f95e9",
+			"3497a9....",
 			"랄로",
-			"http://localhost:8080/zzal/랄로.png",
+			domain + "/zzal/랄로.png",
 			315000
 		);
 		streamerService.register(command);
@@ -55,12 +60,11 @@ public class DataInitializer implements CommandLineRunner {
 
 	private void 파카() {
 		RegisterStreamerCommand command = new RegisterStreamerCommand(
-			"42597020c1a79fb151bd9b9beaa9779b",
+			"42597020c1a7...",
 			"파카",
-			"http://localhost:8080/zzal/파카.jpg",
+			domain + "/zzal/파카.jpg",
 			224000
 		);
 		streamerService.register(command);
 	}
-
 }
