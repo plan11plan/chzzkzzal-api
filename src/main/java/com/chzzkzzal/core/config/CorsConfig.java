5c2702ae52f3284ffff452dbcd64060568ab1935
plan11plan <tjtfy7028@gmail.com>
2025-04-16 02:36:49 +0900

    refactor: cors 프론트 주소 변경

--- src/main/java/com/chzzkzzal/core/config/CorsConfig.java
+++ src/main/java/com/chzzkzzal/core/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowCredentials(true); // 내 서버가 응답할 때 json을 자바스크립트에서 처리할 수 있게 할지 설정
 		// config.addAllowedOrigin("http://localhost:3000"); // 모든 ip에 응답 허용
-		config.addAllowedOrigin(FRONT_DOMAIN + ":3000"); // 모든 ip에 응답 허용
+		config.addAllowedOrigin(FRONT_DOMAIN); // 모든 ip에 응답 허용
 
 		config.addAllowedHeader("*"); // 모든 헤더에 응답 허용
 		config.addAllowedMethod("*"); // 모든 post, get, put, delete, patch 요청 허용
