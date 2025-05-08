ae0da9f3b2439aa0cb2b27144985303649e02da3
plan11plan <tjtfy7028@gmail.com>
2025-05-08 14:55:11 +0900

    chore: 주석 제거

--- src/main/java/com/chzzkzzal/core/auth/application/impl/ChzzkSignInUseCase.java
+++ src/main/java/com/chzzkzzal/core/auth/application/impl/ChzzkSignInUseCase.java
@@ -2,12 +2,10 @@ package com.chzzkzzal.core.auth.application.impl;
 
 import org.springframework.stereotype.Service;
 
-import com.chzzkzzal.common.properties.TokenProperties;
 import com.chzzkzzal.core.auth.application.usecase.SignInUseCase;
 import com.chzzkzzal.core.auth.application.usecase.dto.SignInCommand;
 import com.chzzkzzal.core.auth.domain.repository.SaveMemberPort;
 import com.chzzkzzal.core.auth.domain.service.RefreshTokenService;
-import com.chzzkzzal.core.auth.infrastructure.jwt.TokenInjector;
 import com.chzzkzzal.core.auth.infrastructure.jwt.TokenProvider;
 import com.chzzkzzal.core.auth.web.response.SignInResponse;
 import com.chzzkzzal.member.domain.Member;
@@ -22,19 +20,12 @@ public class ChzzkSignInUseCase implements SignInUseCase {
 	private final SaveMemberPort saveMemberPort;
 	private final RefreshTokenService refreshTokenService;
 	private final TokenProvider tokenProvider;
-	private final TokenInjector tokenInjector;
-	private final TokenProperties tokenProperties;
 
 	@Override
 	public SignInResponse execute(final SignInCommand command) {
 		Member member = saveMemberPort.save(command.channelId(), command.channelName());
 		String accessToken = tokenProvider.generateAccessToken(String.valueOf(member.getId()));
 		String refreshToken = refreshTokenService.issueRefreshToken(member);
-
-		//		tokenInjector.injectRefreshTokenToCookie(new TokenResult(refreshToken, String.valueOf(member.getId())),
-		//			response);
-		//		tokenInjector.addCookie(TokenName.SESSION.name(), accessTokenMinutes,
-		//			(int)tokenProperties.expirationTime().accessTokenMinutes(), response);
 
 		return new SignInResponse(member.getChannelName(), accessToken, refreshToken);
 	}
