6193ad096006ccea8c8a5f9a7a74b5a8f9f45c3c
plan11plan <tjtfy7028@gmail.com>
2025-05-08 12:07:19 +0900

    chore(TokenInjector): 불필요한 주석 제거

--- src/main/java/com/chzzkzzal/core/auth/infrastructure/jwt/TokenInjector.java
+++ src/main/java/com/chzzkzzal/core/auth/infrastructure/jwt/TokenInjector.java
@@ -12,7 +12,6 @@ import com.chzzkzzal.common.properties.SecurityProperties;
 import com.chzzkzzal.common.properties.TokenProperties;
 import com.chzzkzzal.core.auth.domain.dto.TokenResult;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -23,48 +22,28 @@ public class TokenInjector {
 	private final SecurityProperties securityProperties;
 
 	public void injectRefreshTokenToCookie(TokenResult result, HttpServletResponse response) {
-		addCookie(REFRESH_TOKEN.name(), result.refreshToken(),
-			(int)tokenProperties.expirationTime().refreshTokenHours(),
+		addCookie(
+			REFRESH_TOKEN.name(),
+			result.refreshToken(),
+			Duration.ofHours(tokenProperties.expirationTime().refreshTokenHours()),
 			response
 		);
 	}
 
-	public void addCookie(String name, String value, int maxAge, HttpServletResponse response) {
+	public void invalidateCookie(String name, HttpServletResponse response) {
+		addCookie(name, "", Duration.ZERO, response);
+	}
+
+	public void addCookie(String name, String value, Duration duration, HttpServletResponse response) {
 		ResponseCookie cookie = ResponseCookie.from(name, value)
 			.path("/")
-			.maxAge(Duration.ofSeconds(maxAge))
+			.maxAge(duration)
 			.httpOnly(securityProperties.cookie().httpOnly())
 			.domain(securityProperties.cookie().domain())
 			.secure(securityProperties.cookie().secure())
-			.sameSite("none")
+			.sameSite(securityProperties.cookie().sameSite())
 			.build();
 		response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 	}
 
-	//	public HttpServletResponse addCookie(String name, String value, int maxAge, HttpServletResponse response) {
-	//		ResponseCookie cookie = ResponseCookie.from(name, value)
-	//			.path("/")
-	//			.maxAge(Duration.ofSeconds(maxAge))
-	//			.httpOnly(securityProperties.cookie().httpOnly())
-	//			.domain(securityProperties.cookie().domain())
-	//			.secure(securityProperties.cookie().secure())
-	//			.sameSite("none")
-	//			.build();
-	//		response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
-	//		return response;
-	//	}
-
-	public void invalidateCookie(
-		String name,
-		HttpServletResponse response
-	) {
-		Cookie cookie = new Cookie(name, null);
-		cookie.setPath("/");
-		cookie.setMaxAge(0);
-		cookie.setHttpOnly(securityProperties.cookie().httpOnly());
-		cookie.setDomain(securityProperties.cookie().domain());
-		cookie.setSecure(securityProperties.cookie().secure());
-		cookie.setAttribute("SameSite", "None");
-		response.addCookie(cookie);
-	}
 }
