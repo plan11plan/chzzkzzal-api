4d796b36b4975245642aa27edd68f08e252d67b6
plan11plan <tjtfy7028@gmail.com>
2025-05-10 16:52:03 +0900

    feat: 크로스 도메인에서 사용 가능한 API 마크 어노테이션 추가

---
+++ src/main/java/com/chzzkzzal/common/annotation/PublicApi.java
@@ -0,0 +1,14 @@
+package com.chzzkzzal.common.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.CLASS)
+@Documented
+public @interface PublicApi {
+	String value() default "Public API exposed for cross-domain usage.";
+}
