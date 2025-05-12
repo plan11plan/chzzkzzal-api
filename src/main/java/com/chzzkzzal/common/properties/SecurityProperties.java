4c01ed8987604b1b687cbc87ba268c5b92ff5ecd
plan11plan <tjtfy7028@gmail.com>
2025-05-13 00:25:42 +0900

    fix: 시큐리티프러퍼티 내부 구조 수정

--- src/main/java/com/chzzkzzal/common/properties/SecurityProperties.java
+++ src/main/java/com/chzzkzzal/common/properties/SecurityProperties.java
@@ -1,13 +1,14 @@
 package com.chzzkzzal.common.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 @ConfigurationProperties(prefix = "chzzkzzal.security")
-public record SecurityProperties(
-	@NestedConfigurationProperty Cookie cookie
+public record SecurityProperties(Cookie cookie) {
 
-) {
-	public record Cookie(String domain, boolean httpOnly, boolean secure, String sameSite) {
+	public record Cookie(
+		String domain,
+		boolean httpOnly,
+		boolean secure,
+		String sameSite) {
 	}
 }
