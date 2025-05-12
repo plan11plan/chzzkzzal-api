34e6854a63619118d79db7c157b0ee12f7373719
plan11plan <tjtfy7028@gmail.com>
2025-05-12 23:40:54 +0900

    refactor: 참조config prefix 변경

--- src/main/java/com/chzzkzzal/common/properties/SecurityProperties.java
+++ src/main/java/com/chzzkzzal/common/properties/SecurityProperties.java
@@ -3,7 +3,7 @@ package com.chzzkzzal.common.properties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
-@ConfigurationProperties(prefix = "spring.security")
+@ConfigurationProperties(prefix = "chzzkzzal.security")
 public record SecurityProperties(
 	@NestedConfigurationProperty Cookie cookie
 
